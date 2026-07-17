f763bca906016700ac3847d224305b29eea1f626
festusoa <festusopeyemi2019@gmail.com>
2026-07-17 22:58:42 +0100

    xhanged hard_coded database

--- app/db.py
+++ app/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql://postgres:1234@localhost:5432/rbac_db"
+db_url = os.getenv("DATABASE_URL")
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
