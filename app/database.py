71f576fdd2b4a46246156e6e5d3a0781a1ec0c94
festusoa <festusopeyemi2019@gmail.com>
2026-07-18 02:42:56 +0100

    database conflict

--- app/database.py
+++ app/database.py
@@ -1,14 +1,22 @@
-# database.py
 import os
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, declarative_base
+from sqlalchemy.orm import declarative_base
 
+#variables from  .env file
 load_dotenv()
+
+# Database URL
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+#Async engine
 engine = create_async_engine(DATABASE_URL, echo=True)
-AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
-# ONLY USE THIS ONE
+# Session maker for async sessions
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, 
+    expire_on_commit=False
+)
+
+#Base class
 Base = declarative_base()
